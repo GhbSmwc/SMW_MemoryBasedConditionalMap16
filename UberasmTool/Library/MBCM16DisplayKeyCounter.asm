0838e7f11ad61f9a56257c4ff4ddd90ed64825a3
? <GhbSmwc@users.noreply.github.com>
2025-12-16 16:46:56 -0500

    WIP

--- UberasmTool/Library/MBCM16DisplayKeyCounter.asm
+++ UberasmTool/Library/MBCM16DisplayKeyCounter.asm
@@ -19,52 +19,52 @@
 DisplayHud:
 	
 	.ClearTiles
-	LDX.b #(!StatusBarFormat*$03)
+		LDX.b #(!StatusbarFormat*$03)
 	
-	..Loop
-	LDA #!Settings_MBCM16_KeyCounterBlankTileNumb
-	STA !Settings_MBCM16_KeyCounterTileNumbPos,x
-	if !EditTileProps != 0
-		LDA #!Settings_MBCM16_KeyCounterBlankTileProp
-		STA !Settings_MBCM16_KeyCounterTilePropPos,x
-	endif
-	DEX #!StatusBarFormat
-	BPL ..Loop
+		..Loop
+			LDA #!Settings_MBCM16_KeyCounterBlankTileNumb
+			STA !Settings_MBCM16_KeyCounterTileNumbPos,x
+			if !EditTileProps != 0
+				LDA #!Settings_MBCM16_KeyCounterBlankTileProp
+				STA !Settings_MBCM16_KeyCounterTilePropPos,x
+			endif
+			DEX #!StatusbarFormat
+			BPL ..Loop
 	
-	TYX
-	LDA !Freeram_KeyCounter,x
-	BEQ .Done
+		TYX
+		LDA !Freeram_KeyCounter,x
+		BEQ .Done
 	.WriteTilePrefix
-	;Write <keysymbol>X<1 or 2 digits here>
-	LDA #!Settings_MBCM16_KeyCounterKeySymbolTileNumb : STA !Settings_MBCM16_KeyCounterTileNumbPos
-	if !EditTileProps != 0
-		LDA #!Settings_MBCM16_KeyCounterKeySymbolTileProp : STA !Settings_MBCM16_KeyCounterTilePropPos
-	endif
-	LDA #!Settings_MBCM16_KeyCounterXSymbolTileNumb : STA !Settings_MBCM16_KeyCounterTileNumbPos+(!StatusBarFormat*$01)
-	if !EditTileProps != 0
-		LDA #!Settings_MBCM16_KeyCounterXSymbolTileProp : STA !Settings_MBCM16_KeyCounterTilePropPos+(!StatusBarFormat*$01)
-	endif
+		;Write <keysymbol>X<1 or 2 digits here>
+		LDA #!Settings_MBCM16_KeyCounterKeySymbolTileNumb : STA !Settings_MBCM16_KeyCounterTileNumbPos
+		if !EditTileProps != 0
+			LDA #!Settings_MBCM16_KeyCounterKeySymbolTileProp : STA !Settings_MBCM16_KeyCounterTilePropPos
+		endif
+		LDA #!Settings_MBCM16_KeyCounterXSymbolTileNumb : STA !Settings_MBCM16_KeyCounterTileNumbPos+(!StatusbarFormat*$01)
+		if !EditTileProps != 0
+			LDA #!Settings_MBCM16_KeyCounterXSymbolTileProp : STA !Settings_MBCM16_KeyCounterTilePropPos+(!StatusbarFormat*$01)
+		endif
 	
 	.WriteDigits
-	if !EditTileProps != 0
-		LDA.b #!Settings_MBCM16_KeyCounterDigitsTileProp
-		STA !Settings_MBCM16_KeyCounterTilePropPos+(!StatusBarFormat*$02)
-		STA !Settings_MBCM16_KeyCounterTilePropPos+(!StatusBarFormat*$03)
-	endif
+		if !EditTileProps != 0
+			LDA.b #!Settings_MBCM16_KeyCounterDigitsTileProp
+			STA !Settings_MBCM16_KeyCounterTilePropPos+(!StatusbarFormat*$02)
+			STA !Settings_MBCM16_KeyCounterTilePropPos+(!StatusbarFormat*$03)
+		endif
 	
-	LDA !Freeram_KeyCounter,x
-	CMP.b #10
-	BCS ..TwoDigits
+		LDA !Freeram_KeyCounter,x
+		CMP.b #10
+		BCS ..TwoDigits
 	
-	..OneDigit
-	STA !Settings_MBCM16_KeyCounterTileNumbPos+(!StatusBarFormat*$02)
-	RTL
+		..OneDigit
+			STA !Settings_MBCM16_KeyCounterTileNumbPos+(!StatusbarFormat*$02)
+			RTL
 	
-	..TwoDigits
-	JSL $00974C								;>HexDec.
-	STA !Settings_MBCM16_KeyCounterTileNumbPos+(!StatusBarFormat*$03)	;>Ones
-	TXA									;\Tens
-	STA !Settings_MBCM16_KeyCounterTileNumbPos+(!StatusBarFormat*$02)	;/
+		..TwoDigits
+			JSL $00974C								;>HexDec.
+			STA !Settings_MBCM16_KeyCounterTileNumbPos+(!StatusbarFormat*$03)	;>Ones
+			TXA									;\Tens
+			STA !Settings_MBCM16_KeyCounterTileNumbPos+(!StatusbarFormat*$02)	;/
 	.Done
-	RTL
+		RTL
 	
